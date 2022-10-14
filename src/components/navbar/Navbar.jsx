fe9eb625ce205b00854f8764e409ca438d3b1794
ranjeet-meena <ranjeet.meena@u-smart.ai>
2022-10-14 13:34:46 +0530

    navbar created

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../navbar/navbar.css"
+import logo from '../assets/logo.svg'
+function Navbar() {
+  return (
+
+    <nav>
+        <div className="upper-nav">
+            <div className="logo">
+                <img className='logo-img' src={logo} alt="" srcset="" />
+            </div>
+        </div>
+        <div className="leftnav">
+
+        </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
