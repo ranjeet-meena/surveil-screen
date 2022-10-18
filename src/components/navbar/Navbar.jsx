b06b3da4928a03af5aa7aad24a071ac9a85e674e
Dipalikhalate <dipkhalate@gmail.com>
2022-10-18 12:54:11 +0530

    changes login

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,27 +1,48 @@
-import React, { useState } from 'react'
-import "./navbar.css"
-import logo from '../assets/Logo_Smaller Clearspace.svg'
+import React, { useState } from "react";
+import "./navbar.css";
+import logo from "../assets/Logo_Smaller Clearspace.svg";
 function Navbar() {
-    const [classnames, setClassnames] = useState({a:'', b:''})
+  const [classnames, setClassnames] = useState({ a: "", b: "" });
   return (
-
-      <nav className={classnames.b} 
-      onClick={()=>setClassnames({a:'login-success', b:''})} 
-      onDoubleClick={()=>{setClassnames({a:'after-login', b:'after-login'})}} >
-        {/* <div className="upper-nav after-login"> */}
-        <div className={"upper-nav " + classnames.a} >
+    <nav
+      className={classnames.b}
+      onClick={() => setClassnames({ a: "login-success", b: "" })}
+      onDoubleClick={() => {
+        setClassnames({ a: "after-login", b: "after-login" });
+      }}
+    >
+      {/* <div className="upper-nav after-login"> */}
+      <div className={"upper-nav " + classnames.a}>
         {/* <div className="upper-nav"> */}
-            <div className="logo">
-                <img className='logo-img' src={logo} alt="" srcset="" />
-        <div className="leftnav">
-
-        </div>
+        <div className="logo">
+          <img className="logo-img" src={logo} alt="" srcset="" />
+          <div className="leftnav">
+            <div>
+              
             </div>
-        </div>
+            <div>
 
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-
-export default Navbar
+export default Navbar;
